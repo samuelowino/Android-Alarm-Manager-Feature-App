b30725dcd50f57207be4f590504f5f0e23a04255
samuelowino <samuelowino43@yahoo.com>
2020-02-26 17:32:24 +0300

    User interface sketch

--- app/src/main/java/org/kazitek/androidalarm_labs/MainActivity.java
+++ app/src/main/java/org/kazitek/androidalarm_labs/MainActivity.java
@@ -2,13 +2,19 @@ package org.kazitek.androidalarm_labs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.content.Context;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    AlarmManager alarmManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
     }
 }
